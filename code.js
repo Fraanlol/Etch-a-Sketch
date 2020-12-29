a1d9409c36ca0ed9a4697d7b16cb0da77ce6c8b8
Fraanlol <franco.catas@hotmail.com>
2020-12-29 00:55:50 -0300

    Refactoring code

--- code.js
+++ code.js
@@ -4,46 +4,38 @@ let createGridButton = document.querySelector('.gridNumber');
 let colNumber = parseInt(document.querySelector('#gridSquares').value);
 let optionButtons = document.querySelectorAll('.color');
 let clearButton = document.querySelector('.cleanGrid');
-clearButton.addEventListener('click', eraser)
+clearButton.addEventListener('click', eraser);
 let gridToogle = document.querySelector('.toogleGrid');
 gridToogle.addEventListener('click', toogleGridLines);
+let color;
 
-createGridRange.addEventListener('mouseup', () => { /* Creates grid on Desktop */
-    colNumber = parseInt(document.querySelector('#gridSquares').value);
-   drawingSpace.innerHTML = ' '
-   createGridDivs(colNumber);
-   colorPicker();
-})
-
-createGridButton.addEventListener('click', () => { /* Creates grid on Mobile */
-    colNumber = parseInt(document.querySelector('#gridSquares').value);
-    drawingSpace.innerHTML = ' '
-    createGridDivs(colNumber);
-    colorPicker();
- })
-
-
-const createGridDivs = (colNumber) => {
+const createGridDivs = (colNumber) => { //Function to construct grids
     drawingSpace.style.cssText =`grid-template-columns:repeat(${colNumber}, auto); `
     for (i = 0 ; i < `${colNumber**2}` ; i++){
         let square = document.createElement('div');
-        square.style.cssText = 'background-color:white; width:100%; height:100%; -webkit-user-drag: none;'
+        square.style.cssText = 'background-color:white; border:none; margin:0; padding:0; width:100%; height:100%; -webkit-user-drag: none;'
         square.classList.add('pixel')
         drawingSpace.appendChild(square);
     }
     updateGridNumber();
 } 
-createGridDivs(colNumber);
-let color;
 
-function rainbow(){
-    return `rgb(${Math.round(Math.random() * 255)},${Math.round(Math.random() * 255)},${Math.round(Math.random() * 255)})`
+function updateGridNumber(){ //Function to display the actual number of grids
+    colNumber = parseInt(document.querySelector('#gridSquares').value);
+    document.querySelector('.pixelCount').textContent=`${colNumber} x ${colNumber}`;
 }
 
+let newGrid = () => { //Function to create a new grid
+    colNumber = parseInt(document.querySelector('#gridSquares').value);
+    drawingSpace.innerHTML = ' '
+    createGridDivs(colNumber);
+    colorPicker();
+}
 
+createGridRange.addEventListener('mouseup', newGrid);  // Create a grid, using the Range input, available on Desktop
+createGridButton.addEventListener('click', newGrid);  // Create a grid, using the button Create, because Range Input event is not available on Mobile
 
-optionButtons.forEach((i) => {
-   
+optionButtons.forEach((i) => { //This forEach adds a Click event to the OptionButtons and catch wich one was clicked to assign it to a variable
     i.addEventListener('click', () => {
         
         if (i.classList.contains('blackColor')) {
@@ -54,25 +46,24 @@ optionButtons.forEach((i) => {
             return color = 'rainbow';
         } else if (i.classList.contains('eraser')) {
             return color = 'white';
+        } else if (i.classList.contains('pickColor')) { 
+            
         }
     })
 })
 
-
-function colorPicker(){
-    color='black';
-    let x = document.querySelectorAll('.pixel');
-    x.forEach((key) => {
-        let count = 0;
+function colorPicker(){ //Using the variable from the forEach, this function does a forEach on every gridPixel, and adds the event mouseOver to allow draw, and sets the correct color to draw.
+    color='black'; //Here i am setting a default value to color, to use it when the page loads without needing to click o Black button
+    let everyDiv = document.querySelectorAll('.pixel');
+    everyDiv.forEach((key) => {
+        let count = 0; //This count is an internal counter to add opacity in the GreyScale, its value is reset when you pick another color
         key.addEventListener('mouseover' , () => {
-            key.style.border='none';
-          
+            key.style.border='none';          
             if(color == 'rainbow'){
                 key.style.backgroundColor = rainbow();
                 count = 0;
                 key.style.opacity = 1;
-            }else if ( color == 'white'){
-              
+            }else if ( color == 'white'){              
                 key.style.backgroundColor = color;
                 key.style.opacity = 1;
                 count = 0;
@@ -88,10 +79,12 @@ function colorPicker(){
         })
     })
 }
-colorPicker();
 
-function eraser(){
-    
+function rainbow(){ //This function returns a randomColor to make a RainbowEffect, i did it a Function because i need to call it whenever i mouseOver every div
+    return `rgb(${Math.round(Math.random() * 255)},${Math.round(Math.random() * 255)},${Math.round(Math.random() * 255)})`
+}
+
+function eraser(){ //This is a 'Reset' button, i need to call the colorPicker function again because im removing every div
     drawingSpace.innerHTML = ' ';
     createGridDivs(colNumber);
     colorPicker();
@@ -102,7 +95,10 @@ function toogleGridLines() {
     borders.classList.toggle('divBorder')
 }
 
-function updateGridNumber(){
-    colNumber = parseInt(document.querySelector('#gridSquares').value);
-    document.querySelector('.pixelCount').textContent=`${colNumber} x ${colNumber}`;
-}
+createGridDivs(colNumber);
+colorPicker();
+
+
+
+
+
